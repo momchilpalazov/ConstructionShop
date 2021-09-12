bdc07848a749009834a6ffe154457ffe4de6c3e4
momchilpalazov <70229932+momchilpalazov@users.noreply.github.com>
2021-09-12 16:21:38 +0200

    Create Cart Controller and Cart View

---
+++ ConstructionShop/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using ConstructionShop.Data;
+using ConstructionShop.Models;
+using ConstructionShop.Uttility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionShop.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+
+        private readonly ConstructionShopDbContext data;
+
+        public CartController(ConstructionShopDbContext db )
+        {
+            data = db;
+        }
+        public IActionResult Index()
+        {
+            List<BagCart> bagCartsList = new List<BagCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<BagCart>>(WebConstants.CartSession)!=null&&
+                HttpContext.Session.Get<IEnumerable<BagCart>>(WebConstants.CartSession).Count() >0)
+            {
+                //sessionexist
+                bagCartsList = HttpContext.Session.Get<List<BagCart>>(WebConstants.CartSession);
+            }
+            List<int> prodInCart = bagCartsList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productsList = data.Products.Where(u => prodInCart.Contains(u.Id));
+            return View(productsList);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<BagCart> bagCartsList = new List<BagCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<BagCart>>(WebConstants.CartSession) != null &&
+                HttpContext.Session.Get<IEnumerable<BagCart>>(WebConstants.CartSession).Count() > 0)
+            {
+                //sessionexist
+                bagCartsList = HttpContext.Session.Get<List<BagCart>>(WebConstants.CartSession);
+            }
+
+            bagCartsList.Remove(bagCartsList.FirstOrDefault(f => f.ProductId == id));
+            HttpContext.Session.Set(WebConstants.CartSession, bagCartsList);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ ConstructionShop/Models/ViewModels/ProductUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ConstructionShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionShop.Models.ViewModels
+{
+    public class ProductUserViewModel
+    {
+
+        public ProductUserViewModel()
+        {
+            ProductsList = new List<Product>();
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public IEnumerable<Product> ProductsList { get; set; }
+    }
+}
