e70ddbd814f2de5c88cac28c267e27e211618da1
momchilpalazov <70229932+momchilpalazov@users.noreply.github.com>
2021-09-12 17:25:14 +0200

    Create Summary Action and Sumary Page UI

--- ConstructionShop/Controllers/CartController.cs
+++ ConstructionShop/Controllers/CartController.cs
@@ -1,11 +1,13 @@
 ﻿using ConstructionShop.Data;
 using ConstructionShop.Models;
+using ConstructionShop.Models.ViewModels;
 using ConstructionShop.Uttility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ConstructionShop.Controllers
@@ -16,9 +18,13 @@ namespace ConstructionShop.Controllers
 
         private readonly ConstructionShopDbContext data;
 
+        [BindProperty]
+        public ProductUserViewModel ProductUserViewModel { get; set; }
+
         public CartController(ConstructionShopDbContext db )
         {
             data = db;
+            
         }
         public IActionResult Index()
         {
@@ -33,6 +39,44 @@ namespace ConstructionShop.Controllers
             List<int> prodInCart = bagCartsList.Select(i => i.ProductId).ToList();
             IEnumerable<Product> productsList = data.Products.Where(u => prodInCart.Contains(u.Id));
             return View(productsList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+
+            var claimsIdentiy = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentiy.FindFirst(ClaimTypes.NameIdentifier);
+            //var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<BagCart> bagCartsList = new List<BagCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<BagCart>>(WebConstants.CartSession) != null &&
+                HttpContext.Session.Get<IEnumerable<BagCart>>(WebConstants.CartSession).Count() > 0)
+            {
+                //sessionexist
+                bagCartsList = HttpContext.Session.Get<List<BagCart>>(WebConstants.CartSession);
+            }
+            List<int> prodInCart = bagCartsList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productsList = data.Products.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserViewModel = new ProductUserViewModel()
+            {
+                ApplicationUser = data.ApplicationUsers.FirstOrDefault(u => u.Id == claim.Value), ProductsList = productsList
+                 
+
+            };
+
+
+            return View(ProductUserViewModel);
         }
 
         public IActionResult Remove(int id)
@@ -50,5 +94,8 @@ namespace ConstructionShop.Controllers
             HttpContext.Session.Set(WebConstants.CartSession, bagCartsList);
             return RedirectToAction(nameof(Index));
         }
+
+
+       
     }
 }
